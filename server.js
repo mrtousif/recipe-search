3fa37cab7b451a8a8b894561bf80b02989835748
Tousif Islam <tousifislampersonal@outlook.com>
2020-03-26 18:18:28 +0530

    ready to deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+// serve static files
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use("/", (req, res, next) => {
+  res.sendFile("dist/index.html");
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port);
